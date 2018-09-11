06e45638acc6ef1182bad57340eeabf8b888547d
qujian <85113909@qq.com>
2018-09-11 10:10:49 +0800

    教务系统

---
+++ WebContent/r3/anonJs/reJS.js
@@ -0,0 +1,31 @@
+var conf = {
+	apiUrl: "http://www.hngsxy.com/manage/",
+	ticket: {
+		"ticket": "eqw5482e-7we2-4dd1-bdc7-a6fb3e806ddc1"
+	},
+	dbconfig: ""
+};
+(function() {
+	request = {
+		send: function(do_url, param, fun) { // 权限请求处理
+			if(do_url.indexOf("http") == -1) {
+				do_url = conf.apiUrl + do_url + "&_dbCongfig=" + conf.dbconfig;
+			}
+			$.ajax({
+				type: "post",
+				contentType: "application/json",
+				headers: conf.ticket,
+				url: do_url,
+				data: JSON.stringify(param),
+				success: function(data) {
+					if(typeof(fun) == "function") {
+						fun(data);
+					}
+				},
+				error: function(data) {
+					console.log("错误" + data.message);
+				}
+			});
+		}
+	}
+})();
